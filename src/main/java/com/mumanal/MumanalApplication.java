5105459cc7fc6b7e549e5b76d9e81e865bc180d7
Will-Carlo <wil__fer_25@outlook.es>
2026-02-12 18:30:13 -0400

    MONTE CARLO GROUP

---
+++ src/main/java/com/mumanal/MumanalApplication.java
@@ -0,0 +1,13 @@
+package com.mumanal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MumanalApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MumanalApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mumanal/MumanalApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mumanal;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MumanalApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
